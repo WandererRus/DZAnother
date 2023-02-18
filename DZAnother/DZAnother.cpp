f6e41ac6e5c0f79b564805d0f7ade9c3444e1145
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-18 20:46:46 +0300

    Добавьте файлы проекта.

---
+++ DZAnother/DZAnother.cpp
@@ -0,0 +1,50 @@
+﻿// DZAnother.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+    string* massive = new string[2];
+    int numberStudents = 0;
+    cin >> numberStudents;
+    string** massiveAllStudents;
+    if (numberStudents > 0)
+    { 
+        massiveAllStudents = new string * [numberStudents];
+    }
+    else
+    {
+        massiveAllStudents = new string * [2];
+    }
+    for (int i = 0; i < numberStudents; i++)
+    {
+        massiveAllStudents[i] = massive;
+    }
+    for (int i = 0; i < numberStudents; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            string bufer = "";
+            cin >> bufer;
+            massiveAllStudents[i][j] = bufer;
+        }
+    }
+    
+
+    std::cout << "Hello World!\n";
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
